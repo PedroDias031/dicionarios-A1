10cf32b0c3fc5e80c65cd71532386669a36d4eba
Pedro Dias <pedrowest510@gmail.com>
2025-11-19 13:41:48 -0300

    Update dicionarios.py

--- dicionarios.py
+++ dicionarios.py
@@ -10,11 +10,16 @@ cidades = {
     "SP": "São Paulo",
     "PE": "Pernambuco"
     }
+uf = list(cidades.keys())
 
-print('Aqui printa os valores das chaves!')
-for city in cidades.values():
-    print(city)
-    
-print('Aqui printa os chaves do dicionario!')
-for chave in cidades.keys():
-    print(chave)
+
+while True:
+    print('  Informe uma dessas UF para continuar\n AC, BA, CE, DF, MG, PR, RJ, RS, SP, PE.')
+    city = input(' UF desejada: ').upper()
+    if city in uf: 
+        break
+    else:
+        print('Não foi possivel consultar essa UF tente novamente...')
+
+
+print (f'A cidade da UF escolhida e: {cidades[city].upper()}')
